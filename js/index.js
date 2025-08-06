ce2c4bd11b407c112aac21114ed9c41d81fc3acf
Mizantoro <154473705+Mizantoro@users.noreply.github.com>
2025-08-07 01:02:02 +0200

    Added home page

---
+++ js/index.js
@@ -0,0 +1,16 @@
+async function switchTheme() {
+    const root = document.documentElement;
+    if (getComputedStyle(root).getPropertyValue('--background-color').trim()==="#090909") {
+        root.style.setProperty('--background-color', '#FFFAFA');
+        root.style.setProperty('--background-color-higher', '#ededed');
+        root.style.setProperty('--text-color', 'black');
+        document.getElementById("theme_toggle_img").src="media/pictures/index/ico/night.svg";
+    }
+    else {
+        root.style.setProperty('--background-color', '#090909');
+        root.style.setProperty('--background-color-higher', '#121212');
+        root.style.setProperty('--text-color', '#fff');
+        document.getElementById("theme_toggle_img").src="media/pictures/index/ico/day.svg";
+    }
+
+}
