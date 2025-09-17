21aaae22840efd56a73b2584977fe99a10a813d1
Mizantoro <154473705+Mizantoro@users.noreply.github.com>
2025-09-17 23:34:23 +0200

    Vision images will preload

--- js/vision.js
+++ js/vision.js
@@ -1,7 +1,27 @@
+const imagesToPreload = [
+    'media/pictures/vision/astigmatism.jpg',
+    'media/pictures/vision/cataract.jpg',
+    'media/pictures/vision/clear.jpg',
+    'media/pictures/vision/glaucoma.jpg',
+    'media/pictures/vision/hyperopia.jpg',
+    'media/pictures/vision/macular_degeneration.jpg',
+    'media/pictures/vision/myopia.jpg',
+    'media/pictures/vision/retinopathy1.jpg',
+    'media/pictures/vision/retinopathy2.jpg',
+    'media/pictures/vision/visibility.svg'
+];
+
 async function changeImage(img) {
     document.querySelector('h1').style.opacity = 0;
     document.querySelector('a').style.opacity = 0;
     document.body.style.backgroundImage = "url('media/pictures/vision/" + img + "')";
+}
+
+window.onload = function preloadImages() {
+    imagesToPreload.forEach(src => {
+        const img = new Image();
+        img.src = src;
+    });
 }
 
 async function hideButtons() {
