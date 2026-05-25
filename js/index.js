c4b1ea3f39cc174a836d5582af912dd2712a35e8
Mizantoro <paulwespl@gmail.com>
2026-05-26 00:40:26 +0200

    Improve landing page styling

--- js/index.js
+++ js/index.js
@@ -203,7 +203,6 @@ async function switchTheme() {
         root.style.setProperty('--background-color', '#FFFAFA');
         root.style.setProperty('--background-color-higher', '#ededed');
         root.style.setProperty('--text-color', 'black');
-
         document.getElementById("theme_toggle_img").src="media/pictures/index/ico/night.svg";
     }
     else {
